4411e581c71aa70b5b5509d710ba0ba8592d0f01
Alejandro Carrasco Espinoza <alejandro.carrasco.e90@gmail.com>
2025-07-06 01:37:06 -0400

    chore: improve comments

--- examples/webpack-hot-middleware-ssr/src/ErrorTester.jsx
+++ examples/webpack-hot-middleware-ssr/src/ErrorTester.jsx
@@ -4,7 +4,7 @@ export const ErrorTester = () => {
   useEffect(() => {
     // If the error occurs before the first refresh is performed,
     // the boundary error cannot be recovered and will be stuck.
-    // Comment and uncomment this line to test error handling
+    // Comment and uncomment next line to test error handling
     throw new Error('Test');
   }, []);
 
--- examples/webpack-hot-middleware-ssr/src/index.jsx
+++ examples/webpack-hot-middleware-ssr/src/index.jsx
@@ -2,7 +2,7 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 
-// Uncomment this line to apply the fix
+// Uncomment next line to apply the fix
 // window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.forEach((renderer) => renderer.setRefreshHandler(() => undefined));
 
 hydrateRoot(
