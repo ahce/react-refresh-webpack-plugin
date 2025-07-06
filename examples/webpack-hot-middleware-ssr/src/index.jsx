041c789ed82c6c9797a47c7a1632bc34fb98d946
Alejandro Carrasco Espinoza <alejandro.carrasco.e90@gmail.com>
2025-07-06 01:35:08 -0400

    chore: apply fix

--- examples/webpack-hot-middleware-ssr/src/index.jsx
+++ examples/webpack-hot-middleware-ssr/src/index.jsx
@@ -2,6 +2,9 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 
+// Uncomment this line to apply the fix
+// window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.forEach((renderer) => renderer.setRefreshHandler(() => undefined));
+
 hydrateRoot(
   document.getElementById('app'),
   <ErrorBoundary>
